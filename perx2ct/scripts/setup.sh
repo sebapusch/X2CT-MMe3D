129a030fa729253a7c70bfc8334368956f74c78c
Sebastian Pusch <seba0808@gmail.com>
2025-05-11 17:36:46 +0200

    fix paths

--- perx2ct/scripts/setup.sh
+++ perx2ct/scripts/setup.sh
@@ -3,11 +3,12 @@
 set -e
 
 REPO_URL="https://github.com/dek924/PerX2CT.git"
-TARGET_DIR="../PerX2CT"
+TARGET_DIR="./PerX2CT"
 PATCH_DIR="patches"
 CONFIG_DIR="configs"
 CHECKPOINT_DIR="checkpoints"
 
+cd '..'
 
 # Clone original repo if missing
 if [ ! -d "$TARGET_DIR" ]; then
@@ -19,22 +20,22 @@ fi
 
 # Apply patches
 echo "Applying patches"
-cp "$PATCH_DIR/model.py" "$TARGET_DIR/x2ct_nerf/modules/inr/model.py"
-cp "$PATCH_DIR/INREncoderZoomAxisInAlign.py" "$TARGET_DIR/x2ct_nerf/modules/INREncoderZoomAxisInAlign.py"
+cp "./$PATCH_DIR/model.py" "$TARGET_DIR/x2ct_nerf/modules/inr/model.py"
+cp "./$PATCH_DIR/INREncoderZoomAxisInAlign.py" "$TARGET_DIR/x2ct_nerf/modules/INREncoderZoomAxisInAlign.py"
 
 echo "Copying inference scripts"
-cp "../inference.py" "$TARGET_DIR"
-cp "../save_to_volume.py" "$TARGET_DIR"
-cp "../generate_synthetic_volumes.py" "$TARGET_DIR"
+cp "inference.py" "$TARGET_DIR"
+cp "save_to_volume.py" "$TARGET_DIR"
+cp "generate_synthetic_volumes.py" "$TARGET_DIR"
 
 
 # Copy config
 echo "Copying config"
-cp "../$CONFIG_DIR/PerX2CT.yaml" "$TARGET_DIR/$CONFIG_DIR/PerX2CT.yaml"
+cp "./$CONFIG_DIR/PerX2CT.yaml" "$TARGET_DIR/$CONFIG_DIR/PerX2CT.yaml"
 
 # Copy checkpoint
 echo "Copying checkpoint"
 mkdir "$TARGET_DIR/$CHECKPOINT_DIR"
-cp "../$CHECKPOINT_DIR/PerX2CT.ckpt" "$TARGET_DIR/$CHECKPOINT_DIR/PerX2CT.ckpt"
+cp "./$CHECKPOINT_DIR/PerX2CT.ckpt" "$TARGET_DIR/$CHECKPOINT_DIR/PerX2CT.ckpt"
 
 echo "PerX2CT correctly set up at $TARGET_DIR"
