b146776b3541e0d59fd3d3e5e1db7deb0f2a2ff2
Sebastian Pusch <seba0808@gmail.com>
2025-05-11 16:37:36 +0200

    fix paths, file extensions and create subdirectory

--- perx2ct/scripts/setup.sh
+++ perx2ct/scripts/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 REPO_URL="https://github.com/dek924/PerX2CT.git"
-TARGET_DIR="./PerX2CT"
+TARGET_DIR="../PerX2CT"
 PATCH_DIR="patches"
 CONFIG_DIR="configs"
 CHECKPOINT_DIR="checkpoints"
@@ -23,18 +23,18 @@ cp "$PATCH_DIR/model.py" "$TARGET_DIR/x2ct_nerf/modules/inr/model.py"
 cp "$PATCH_DIR/INREncoderZoomAxisInAlign.py" "$TARGET_DIR/x2ct_nerf/modules/INREncoderZoomAxisInAlign.py"
 
 echo "Copying inference scripts"
-cp "./inference.py" "$TARGET_DIR"
-cp "./save_to_volume.py" "$TARGET_DIR"
-cp "./generate_synthetic_volumes.py" "$TARGET_DIR"
+cp "../inference.py" "$TARGET_DIR"
+cp "../save_to_volume.py" "$TARGET_DIR"
+cp "../generate_synthetic_volumes.py" "$TARGET_DIR"
 
 
 # Copy config
 echo "Copying config"
-cp "./$CONFIG_DIR/PerX2CT.yaml" "$TARGET_DIR/$CONFIG_DIR/PerX2CT.yaml"
+cp "../$CONFIG_DIR/PerX2CT.yaml" "$TARGET_DIR/$CONFIG_DIR/PerX2CT.yaml"
 
 # Copy checkpoint
 echo "Copying checkpoint"
 mkdir "$TARGET_DIR/$CHECKPOINT_DIR"
-cp "./$CHECKPOINT_DIR/PerX2CT.ckpt" "$TARGET_DIR/$CHECKPOINT_DIR/PerX2CT.ckpt"
+cp "../$CHECKPOINT_DIR/PerX2CT.ckpt" "$TARGET_DIR/$CHECKPOINT_DIR/PerX2CT.ckpt"
 
 echo "PerX2CT correctly set up at $TARGET_DIR"
