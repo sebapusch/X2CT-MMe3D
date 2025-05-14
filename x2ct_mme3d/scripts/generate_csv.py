0fad378f0da66702f6da6250c8bc1793aff70be0
Sebastian Pusch <seba0808@gmail.com>
2025-05-14 10:28:28 +0200

    include train/test split in csv generation

--- x2ct_mme3d/scripts/generate_csv.py
+++ x2ct_mme3d/scripts/generate_csv.py
@@ -1,6 +1,9 @@
-import os.path
+from os.path import join, dirname, abspath
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+TRAIN = 0.9
 
 KEYWORDS = [
     'atelectasis',
@@ -59,30 +62,32 @@ MISSING_FILES = [
     3593,
 ]
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+PROJECT_ROOT = abspath(join(dirname(__file__), '..', '..'))
 
-RAW_BASE_DIR = os.path.join(PROJECT_ROOT, 'data', 'raw')
-PROCESSED_BASE_DIR = os.path.join(PROJECT_ROOT, 'data', 'processed')
+RAW_BASE_DIR = join(PROJECT_ROOT, 'data', 'raw')
+PROCESSED_BASE_DIR = join(PROJECT_ROOT, 'data', 'processed')
 
-df = pd.read_csv(os.path.join(RAW_BASE_DIR, 'indiana_reports.csv'))
-projections_df = pd.read_csv(os.path.join(RAW_BASE_DIR, 'indiana_projections.csv'))
+df = pd.read_csv(join(RAW_BASE_DIR, 'indiana_reports.csv'))
+projections_df = pd.read_csv(join(RAW_BASE_DIR, 'indiana_projections.csv'))
 
 labels = []
 filtered_rows = []
 
+skipped = 0
+
 for idx, row in df.iterrows():
     if int(row['uid']) in MISSING_FILES:
-        print('file known to be missing, skipping')
+        skipped += 1
         continue
 
     projection = projections_df[(projections_df['uid'] == row['uid']) & (projections_df['projection'] == 'Frontal')]
     if len(projection) == 0:
-        print('missing frontal projection, skipping')
+        skipped += 1
         continue
 
     projection = projections_df[(projections_df['uid'] == row['uid']) & (projections_df['projection'] == 'Lateral')]
     if len(projection) == 0:
-        print('missing lateral projection, skipping')
+        skipped += 1
         continue
 
     problem = str(row['Problems']).lower()
@@ -99,10 +104,25 @@ for idx, row in df.iterrows():
 filtered_df = pd.DataFrame(filtered_rows)
 filtered_df['disease'] = labels
 
-disease = len(filtered_df[filtered_df['disease'] == 1])
+train, test = train_test_split(
+    filtered_df,
+    test_size=1 - TRAIN,
+    stratify=labels,
+    shuffle=True,
+)
 
-print('disease:', disease, 'normal:', len(filtered_df) - disease)
+disease_len = len(filtered_df[filtered_df['disease'] == 1])
 
-output_path = os.path.join(PROCESSED_BASE_DIR, 'indiana_reports.csv')
+print('total:   ', len(filtered_df))
+print('----------------------')
+print('normal:  ', len(filtered_df) - disease_len)
+print('disease: ', disease_len)
+print('----------------------')
+print('skipped: ', skipped)
+print('----------------------')
+print('train:   ', len(train))
+print('test:    ', len(test))
 
-filtered_df.to_csv(output_path, index=False)
+filtered_df.to_csv(join(PROCESSED_BASE_DIR, 'indiana_reports.csv'), index=False)
+train.to_csv(join(PROCESSED_BASE_DIR, 'indiana_reports.train.csv'), index=False)
+test.to_csv(join(PROCESSED_BASE_DIR, 'indiana_reports.test.csv'), index=False)
