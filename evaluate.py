561208bec62be177b8f070e3a4fe6afb2d46438e
Sebastian Pusch <seba0808@gmail.com>
2025-06-02 13:12:59 +0200

    implement evaluation script

---
+++ evaluate.py
@@ -0,0 +1,86 @@
+from argparse import Namespace, ArgumentParser, BooleanOptionalAction
+from os import path
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import pandas as pd
+
+from x2ct_mme3d.data.dataset import XRayDataset, X2CTDataset
+from x2ct_mme3d.models.classifiers import BiplanarCheXNet, X2CTMMed3D
+
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+BATCH_SIZE = 8
+
+
+def _load_model(args: Namespace) -> nn.Module:
+    if args.baseline:
+        model = BiplanarCheXNet(False)
+    else:
+        model = X2CTMMed3D(False)
+
+    state_dict = torch.load(args.checkpoint)
+    model.load_state_dict(state_dict)
+    model.to(DEVICE)
+    model.eval()
+
+    return model
+
+def _load_dataset(args: Namespace) -> DataLoader:
+    init = {
+        'reports_csv_path': args.reports,
+        'projections_csv_path': args.projections,
+        'xray_dir': args.xray_dir,
+    }
+    if args.baseline:
+        dataset = XRayDataset(**init)
+    else:
+        dataset = X2CTDataset(**init, ct_dir=args.ct_dir)
+
+    return DataLoader(dataset,
+                      shuffle=False,
+                      batch_size=args.batch_size,
+                      num_workers=4,
+                      pin_memory=True)
+
+def main(args: Namespace):
+    assert path.exists(args.checkpoint)
+    assert path.exists(args.reports)
+    assert path.exists(args.projections)
+    assert path.exists(path.dirname(args.out_dir))
+
+    model = _load_model(args)
+    dataset = _load_dataset(args)
+
+    results = {
+        'uids': [],
+        'true': [],
+        'pred': [],
+    }
+
+    with torch.no_grad():
+        for inputs, labels in dataset:
+            inputs = {k: v.to(DEVICE) for k, v in ['ct', 'frontal', 'lateral']}
+
+        output = model(inputs)
+
+        results['uids'].extend(inputs['uids'])
+        results['true'].extend(labels.cpu().numpy())
+        results['pred'].extend(torch.sigmoid(output))
+
+    pd.DataFrame(results).to_csv(args.csv_output_path, index=False)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--checkpoint', type=str, required=True)
+    parser.add_argument('--reports', type=str, required=True)
+    parser.add_argument('--projections', type=str, required=True)
+    parser.add_argument('--ct-dir', type=str, required=True)
+    parser.add_argument('--xray-dir', type=str, required=True)
+    parser.add_argument('--batch-size', type=int, default=BATCH_SIZE)
+    parser.add_argument('--baseline', default=False, action=BooleanOptionalAction)
+    parser.add_argument('--csv-out-path', type=str, required=True)
+
+    main(parser.parse_args())
--- x2ct_mme3d/data/dataset.py
+++ x2ct_mme3d/data/dataset.py
@@ -18,9 +18,11 @@ XRAY_TARGET_SIZE = (512, 512)
 class BaseDataset(Dataset, ABC):
     def __init__(self,
                  reports_csv_path: str,
-                 projections_csv_path: str):
+                 projections_csv_path: str,
+                 include_uid: bool = False):
         self.reports = pd.read_csv(reports_csv_path)
         self.projections = pd.read_csv(projections_csv_path)
+        self.include_uid = include_uid
 
     def __len__(self) -> int:
         return len(self.reports)
@@ -56,7 +58,7 @@ class XRayDataset(BaseDataset):
     def __getitem__(self, ix: int) -> (dict[str, Tensor], Tensor):
         report = self.reports.iloc[ix]
 
-        imgs = {}
+        data = {}
         for proj in ['Frontal', 'Lateral']:
             projection = self.projections[
                 (self.projections['uid'] == report['uid']) &
@@ -64,9 +66,12 @@ class XRayDataset(BaseDataset):
                 ].iloc[0]
 
             img = Image.open(os.path.join(self.xray_dir, projection['filename']))
-            imgs[proj.lower()] = self.preprocess(img)
+            data[proj.lower()] = self.preprocess(img)
 
-        return imgs, torch.tensor(report['disease'], dtype=torch.long)
+        if self.include_uid:
+            data['uid'] = report['uid']
+
+        return data, torch.tensor(report['disease'], dtype=torch.long)
 
 
 class X2CTDataset(XRayDataset, CtDataset):
@@ -87,6 +92,8 @@ class X2CTDataset(XRayDataset, CtDataset):
         ct, label = CtDataset.__getitem__(self, ix)
 
         data['ct'] = ct
+        if self.include_uid:
+            data['uid'] = label
 
         return data, label
 
